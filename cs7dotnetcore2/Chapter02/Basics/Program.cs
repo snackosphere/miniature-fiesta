79ecdacf909d7ec6d5b9197397cef52b7bde0180
Taylor Chirillo <taylor.chirillo@gmail.com>
2018-01-02 23:49:47 -0800

    chapter 02 example of reflection, types and methods

--- cs7dotnetcore2/Chapter02/Basics/Program.cs
+++ cs7dotnetcore2/Chapter02/Basics/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 
 namespace Basics
 {
@@ -6,7 +8,27 @@ namespace Basics
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // loop through the assemblies that this application references
+            foreach (var r in Assembly.GetEntryAssembly().GetReferencedAssemblies())
+            {
+                // load the assembly so we can read its details
+                var a = Assembly.Load(new AssemblyName(r.FullName));
+
+                // declare a varialbe to count the total number of methods
+                int methodCount = 0;
+
+                // loop through all the types in the assembly
+                foreach (var t in a.DefinedTypes)
+                {
+                    // add up the counts of methods
+                    methodCount += t.GetMethods().Count();
+                }
+
+                // output the count of types and their methods
+                Console.WriteLine($"{a.DefinedTypes.Count():N0} types " + $"with {methodCount:N0} methods in {r.Name} assembly.");
+
+                
+            }
         }
     }
 }
