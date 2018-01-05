9f8530a31e8210d97b77fd0bb1171d89c215e392
Taylor Chirillo <taylor.chirillo@gmail.com>
2018-01-05 09:39:27 -0800

    create numbers example

--- cs7dotnetcore2/Chapter02/Numbers/Program.cs
+++ cs7dotnetcore2/Chapter02/Numbers/Program.cs
@@ -6,7 +6,9 @@ namespace Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"int uses {sizeof(int)} bytes and can store numbers in the range of {int.MinValue:N0} to {int.MaxValue:N0}.");
+            Console.WriteLine($"double uses {sizeof(double)} bytes and can store numbers in the range of {double.MinValue:N0} to {double.MaxValue:N0}.");
+            Console.WriteLine($"decimal uses {sizeof(decimal)} bytes and can store numbers in the range of {decimal.MinValue:N0} to {decimal.MaxValue:N0}.");
         }
     }
 }
