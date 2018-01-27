0c961ff930d58ab3cfccd26d166ac14315c94fda
Taylor Chirillo <taylor.chirillo@gmail.com>
2018-01-26 21:26:57 -0800

    add commands to install Dart

--- ubuntuSetup/ubuntuSetup.sh
+++ ubuntuSetup/ubuntuSetup.sh
@@ -8,7 +8,7 @@ dconf write /org/gnome/shell/extensions/dash-to-dock/dash-max-icon-size 16
 sudo apt-get update
 
 # Add essential development packages
-sudo apt-get install curl git vim
+sudo apt-get install apt-transport-https curl git vim
 
 # Add Visual Studio Code repository and keys
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
@@ -17,6 +17,16 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode s
 
 # Install Visual Studio Code
 sudo apt-get install Code
+
+# Add the Google Linux signing key and Dart apt repository
+sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+
+# Install Dart
+sudo apt-get install dart
+
+# Add Dart to path
+echo "export PATH /usr/lib/dart/bin:$PATH"
 
 # Configure Git identity
 git config --global user.name "Taylor Chirillo"
