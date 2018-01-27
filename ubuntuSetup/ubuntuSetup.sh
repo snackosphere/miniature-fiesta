fb372ba4cbd686903dc2d8e44ed1880d331637a0
Taylor Chirillo <taylor.chirillo@gmail.com>
2018-01-26 22:12:40 -0800

    add google cloud sdk

--- ubuntuSetup/ubuntuSetup.sh
+++ ubuntuSetup/ubuntuSetup.sh
@@ -15,8 +15,21 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
+### Add Google Cloud Repo
+# Create an environment variable for the correct distribution
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+
+# Add the Cloud SDK distribution URI as a package source
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+
+# Import the Google Cloud Platform public key
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
 # Install Visual Studio Code
-sudo apt-get install Code
+sudo apt-get install code
+
+# Install Google Cloud SDK
+sudo apt-get install google-cloud-sdk
 
 # Add the Google Linux signing key and Dart apt repository
 sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
