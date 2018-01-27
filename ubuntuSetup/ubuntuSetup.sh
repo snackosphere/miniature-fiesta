952c4ee20b316732f5fb9d75c7d5eefa7e3cae46
Taylor Chirillo <taylor.chirillo@gmail.com>
2018-01-26 20:58:59 -0800

    add curl to essential packages

--- ubuntuSetup/ubuntuSetup.sh
+++ ubuntuSetup/ubuntuSetup.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 # My configuration for a fresh Ubuntu install
 
+# Update package lists
+sudo apt-get update
+
+# Add essential development packages
+sudo apt-get install curl git vim
+
 # Add Visual Studio Code repository and keys
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-# Update package lists
-sudo apt-get update
-
-# Add essential development packages
-sudo apt-get install code git vim
+# Install Visual Studio Code
+sudo apt-get install Code
 
 # Configure Git identity
 git config --global user.name "Taylor Chirillo"
