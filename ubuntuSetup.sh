a4f6756109a43b44aae19fcdd541b33dfd8d1252
Taylor Chirillo <taylor.chirillo@gmail.com>
2018-01-26 18:19:25 -0800

    turn on credential helper

--- ubuntuSetup.sh
+++ ubuntuSetup.sh
@@ -7,9 +7,13 @@ sudo apt-get update
 # Add essential development packages
 sudo apt-get install vim git 
 
-# Configure Git
+# Configure Git identity
 git config --global user.name "Taylor Chirillo"
 git config --global user.email taylor.chirillo@gmail.com
+
+# Turn on credential helper and set timeout to 1 hour
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
 
 # Display Git settings
 git config user.name
